49c85ff0542c51b54bbed0f9705399877e77056e
Christian Gerlach <christianrgerlach@gmail.com>
2018-03-24 02:09:21 -0700

    basic work in place

--- speedmap.py
+++ speedmap.py
@@ -1,8 +1,10 @@
 from ansible.runner import Runner
 
-servers = ['raspi', 'proxbox', 'mint']
+servers = ['robotch', 'raspi', 'mint' ]
 
 clients = servers
+
+tests = 0
 
 # for every server
 for server in servers:
@@ -16,37 +18,57 @@ for server in servers:
 		pattern = server
 		).run()
 
-	#for every client
+	# for every client
 	for client in clients:
-		print ('***** testing between ' + client + ' to ' + server + '*****') 
-		#ping from client to server
+		tests += 1
+		print ('\n***** testing between client:' + client + ' and server:' + server + ' *****') 
+		# ping from client to server
 		ping_cmd = 'ping -c 1 ' + server + ' | tail -1 | awk -F \'/\' \'{print $5}\''
-
+		#print('start ansible')
 		results = Runner(
 			module_name = 'shell',
 			module_args = ping_cmd,
 			pattern = client
 			).run()
-
+		#print('ansible done')
 		ping_stdout = results['contacted'][client]['stdout']
 		ping_stderr = results['contacted'][client]['stderr']
 
 		latency = ping_stdout
-		print ('latency from ' + client + ' to ' + server + ': ' + latency + 'ms')
-		print (ping_stderr)
+		
+		if ping_stderr:
+			print (ping_stderr)
+		else:
+			print ('latency: ' + latency + ' ms')
 
-		#iperf from client to server
-		iperf_cmd = 'iperf -t 1 -c ' + server + ' | tail -1 | sed \'s/.*\(...............\)/\\1/\''
-
+		# iperf from client to server
+		iperf_cmd = 'iperf -t 1 -c ' + server + ' | tail -1 | sed \'s/.*\(...............\)/\\1/\' | sed \'s/^[ \\t]*//\''
+		#print('start ansible')
 		results = Runner(
 			module_name = 'shell',
 			module_args = iperf_cmd,
 			pattern = client
 			).run()		
-
+		#print('ansible done')
 		iperf_stdout = results['contacted'][client]['stdout']
 		iperf_stderr = results['contacted'][client]['stderr']
 
 		throughput = iperf_stdout
-		print ('throughput from ' + client + ' to ' + server + ': ' + throughput)
-		print (ping_stderr)
+		
+		if iperf_stderr:
+			print (iperf_stderr)
+		else:
+			print ('throughput: ' + throughput)
+
+	#kill iperf server
+	results = Runner(
+		module_name = 'shell',
+		module_args = 'kill -9 $(pgrep iperf)',
+		pattern = server
+		).run()
+
+	iperf_s_killer_stdout = results['contacted'][server]['stdout']
+	iperf_s_killer_stdout = results['contacted'][server]['stderr']
+
+
+
